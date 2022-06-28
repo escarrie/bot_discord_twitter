566edf13aa82681d09df4b97942ff657480c7af7
Neyrods <fusoreur@gmail.com>
2022-06-28 15:42:08 +0200

    [REF]

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const { Client, Collection } = require("discord.js");
 const colors = require("colors");
 const config = require("./config.json");
+const TwitterApi = require("twitter-api-v2");
+
+console.log(`=====================`.bold.red)
+console.log(`API TWITTER CONECTION`.bold.blue)
+console.log(`=====================`.bold.red)
+const twitterClient = new TwitterApi(`${config.twitter_token}`);
 
 const client = new Client({
   intents: 32767, // All intents
